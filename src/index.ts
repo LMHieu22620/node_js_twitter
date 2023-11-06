5a4736395d52e452703ad26df8abddce1100da9e
MinhHieu <minhhieu@gmail.com>
2023-11-06 22:07:38 +0700

    handle error express

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-import express from 'express'
+import express,{Response,NextFunction, Request} from 'express'
 import usersRouter from '~/routes/users.routes'
 import databaseService from './services/database.services'
 const app = express()
 const port = 3002
 app.use(express.json())
 app.use('/users', usersRouter)
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log('lỗi là ', err.message);
+  res.status(400).json({ error: err.message })
+})
 databaseService.connect()
 
 app.listen(port, () => {
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,11 @@
 import { Request, Response, Router } from 'express'
 import { loginController, registerController } from '~/controllers/users.controllers'
 import { loginValidator, registerValidatorSchema } from '~/middlewares/users.middlewares'
+import { wrapRequestHandler } from '~/utils/handlers'
 
 const usersRouter = Router()
 
 usersRouter.post('/login', loginValidator, loginController)
-usersRouter.post('/register', registerValidatorSchema, registerController)
+usersRouter.post('/register', registerValidatorSchema, wrapRequestHandler(registerController))
 
 export default usersRouter
